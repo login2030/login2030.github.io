a0936a1d8a4cd2b01ebbee3b59833d02811ac986
login2030 <login2030@gmail.com>
2017-11-05 14:54:46 +0500

    add AkcentufaCalcPolygraphy

--- src/data/scripts.js
+++ src/data/scripts.js
@@ -4,6 +4,13 @@ export default [
 		header: 'SVG Карта клуба RIO',
 		desc: 'Интерактивная карта фитнес-клуба RIO',
 		// component: 'RiofitnessSvgMap',
-		staticPage: '/static-scripts/RiofitnessSvgMap.html'
+		staticPage: '/static-scripts/RiofitnessSvgMap'
+	},
+	{
+		img: 'akcentufa-calc-polygraphy',
+		header: 'Калькулятор услуг полиграфии',
+		desc: 'Калькулятор услуг полиграфии для рекламного агентства "Акцент" г. Уфа',
+		// component: 'RiofitnessSvgMap',
+		staticPage: '/static-scripts/AkcentufaCalcPolygraphy'
 	},
 ];
