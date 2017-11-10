a3a5efcb860343c8d25ab2b9804ff7279945478b
login2030 <login2030@gmail.com>
2017-11-10 23:58:41 +0500

    --

--- iframe-sites/iframe.js
+++ iframe-sites/iframe.js
@@ -1,4 +1,4 @@
 window.addEventListener('load', function() {
 	var iframe = document.getElementById('iframe-site');
-	iframe.src = iframe.src.replace("http","https");
+	iframe.src = iframe.src.replace("https","http");
 });
